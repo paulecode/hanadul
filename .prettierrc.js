e2a025cccc51e8d22390864e026185d04f66c634
Paul Ebert <paul.ebert@code.berlin>
2025-04-21 17:07:50 +0200

    init

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+import { rules } from 'eslint-config-prettier';
+
+const config = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 100,
+};
+
+export default config;
